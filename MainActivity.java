257e571957fe672949936c5ec60c694cdd8ef196
alvaromuros27 <100934385+alvaromuros27@users.noreply.github.com>
2022-10-20 18:40:49 +0200

    ADD functions button aceptar

---
+++ MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.formulario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    public ArrayList<String> nombres = new ArrayList<>();
+    public ArrayList<String> apellidos = new ArrayList<>();
+    public ArrayList<String> email = new ArrayList<>();
+    public ArrayList<String> mensaje = new ArrayList<>();
+    public ArrayList<String> suscrito = new ArrayList<String>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textonombre = findViewById(R.id.textnombre);
+        TextView textapellidos = findViewById(R.id.textapellidos);
+        TextView textemail = findViewById(R.id.textemail);
+        TextView textmensaje = findViewById(R.id.textmensaje);
+        CheckBox subcribirse = findViewById(R.id.sub);
+        Button button = (Button) findViewById(R.id.aceptar);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String a = textonombre.getText().toString();
+                nombres.add(a);
+
+                String b = textapellidos.getText().toString();
+                apellidos.add(b);
+
+                String c = textemail.getText().toString();
+                email.add(c);
+
+                String d = textmensaje.getText().toString();
+                mensaje.add(d);
+
+                boolean e = subcribirse.getLinksClickable();
+
+            }
+        });
+    }
+
+
+
+}
